660135da167a246157cdb56d5a7ec294fd9380d6
kagura <mackerelsama@gmail.com>
2025-03-09 17:43:13 -0400

    add crash and hang info

--- demo.cpp
+++ demo.cpp
@@ -11,8 +11,10 @@ int main() {
    getline(cin, str);
 
     if(str[0] == 0) {
+        cout << "<-------crash point 1------->" << endl;
         abort(); // crash1
     } else if (str[str.length() - 1] == 0) {
+        cout << "<-------hang point 1------->" << endl;
         sleep(10); // hang1
     }
 
@@ -22,8 +24,10 @@ int main() {
         char c = str[count];
         if(c >= '0' && c <= '9') {
             if(c == prev_num + 1) {
+                cout << "<-------crash point 2------->" << endl;
                 abort(); // crash2
             } else if (c == prev_num + 2) {
+                cout << "<-------hang point 2------->" << endl;
                 sleep(10); // hang2
             }
             prev_num = c;
